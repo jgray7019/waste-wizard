917b8116e306320eb41b4b53a00b123c4424dadf
Jennifer Gray <jgray7019@gmail.com>
2017-11-22 18:38:20 -0500

    added search functionality

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,20 +1,78 @@
-const wasteApp = {};
+var wasteApp = {};
 
-wasteApp.getWasteData = function(){
+
+const wasteItems = [];
+
+wasteApp.events = function () {
+	// on form submit
+	// wasteApp.onFormSubmitListener();
+
+	wasteApp.getWasteData();
+	// wasteApp.findMatchingItems();
+};
+
+// on click of search button, ajax call takes place to find data string that matches user's input
+// wasteApp.onFormSubmitListener = function () {
+// 	$('.main__formSearchBtn').on('click', function(e) {
+// 		e.preventDefault();
+
+// 	})
+// };
+
+wasteApp.getWasteData = function () {
 	$.ajax({
 		url: './public/data/data.json',
 		method: 'GET',
 		dataType: 'json'
-	}).then(function(res) {
-		console.log(res);
-	})
+	}).then(function (res) {
+		// console.log(res);
+		wasteItems.push(...res);
+		console.log(wasteItems);
+	});
 };
 
+
+wasteApp.findMatchingItems = function (wordToMatch, wasteItems) {
+	return wasteItems.filter(wasteItem => {
+		const regex = new RegExp(wordToMatch, 'gi');
+
+		return wasteItem.ALT_WORDS.match(regex) || wasteItem.TITLE.match(regex)
+	});
+}
+
+wasteApp.displayMatchingItems = function() {
+	const matchingItemsArray = wasteApp.findMatchingItems(this.value, wasteItems);
+
+
+	const html = matchingItemsArray.map(wasteItem => {
+		console.log(wasteItem);
+
+	// let wasteTitleEL = $('<span>').text(wasteItem.TITLE);
+	// let wasteAlternateEL = $('<span>').text(wasteItem.ALT_WORDS);
+
+	// let wasteLi = $('<li>').addClass('suggestion').append(wasteAlternateEL, wasteTitleEL);
+	// $('.main_formSuggestions').append(wasteLi);
+		return `
+			<li>
+				<span>${wasteItem.TITLE}</span>
+				<span>${wasteItem.ALT_WORDS}</span>
+			</li>
+		`;
+
+	});
+	
+	$('.main_formSuggestions').html(html);
+
+}
+
+$('.main__searchInput').on('change', wasteApp.displayMatchingItems);
+$('.main__searchInput').on('keyup', wasteApp.displayMatchingItems);
+
 wasteApp.init = function(){
-	wasteApp.getWasteData();
+	wasteApp.events();
 };
+
 
 $(function(){
 	wasteApp.init();
 });
-
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,26 @@
 'use strict';
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var wasteApp = {};
+
+var wasteItems = [];
+
+wasteApp.events = function () {
+	// on form submit
+	// wasteApp.onFormSubmitListener();
+
+	wasteApp.getWasteData();
+	// wasteApp.findMatchingItems();
+};
+
+// on click of search button, ajax call takes place to find data string that matches user's input
+// wasteApp.onFormSubmitListener = function () {
+// 	$('.main__formSearchBtn').on('click', function(e) {
+// 		e.preventDefault();
+
+// 	})
+// };
 
 wasteApp.getWasteData = function () {
 	$.ajax({
@@ -8,12 +28,42 @@ wasteApp.getWasteData = function () {
 		method: 'GET',
 		dataType: 'json'
 	}).then(function (res) {
-		console.log(res);
+		// console.log(res);
+		wasteItems.push.apply(wasteItems, _toConsumableArray(res));
+		console.log(wasteItems);
 	});
 };
 
+wasteApp.findMatchingItems = function (wordToMatch, wasteItems) {
+	return wasteItems.filter(function (wasteItem) {
+		var regex = new RegExp(wordToMatch, 'gi');
+
+		return wasteItem.ALT_WORDS.match(regex) || wasteItem.TITLE.match(regex);
+	});
+};
+
+wasteApp.displayMatchingItems = function () {
+	var matchingItemsArray = wasteApp.findMatchingItems(this.value, wasteItems);
+
+	var html = matchingItemsArray.map(function (wasteItem) {
+		console.log(wasteItem);
+
+		// let wasteTitleEL = $('<span>').text(wasteItem.TITLE);
+		// let wasteAlternateEL = $('<span>').text(wasteItem.ALT_WORDS);
+
+		// let wasteLi = $('<li>').addClass('suggestion').append(wasteAlternateEL, wasteTitleEL);
+		// $('.main_formSuggestions').append(wasteLi);
+		return '\n\t\t\t<li>\n\t\t\t\t<span>' + wasteItem.TITLE + '</span>\n\t\t\t\t<span>' + wasteItem.ALT_WORDS + '</span>\n\t\t\t</li>\n\t\t';
+	});
+
+	$('.main_formSuggestions').html(html);
+};
+
+$('.main__searchInput').on('change', wasteApp.displayMatchingItems);
+$('.main__searchInput').on('keyup', wasteApp.displayMatchingItems);
+
 wasteApp.init = function () {
-	wasteApp.getWasteData();
+	wasteApp.events();
 };
 
 $(function () {
